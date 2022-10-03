1b0ee347defde0c587312b33eebe541be716c79c
kennethryandy <dykennethryan@gmail.com>
2022-10-04 00:54:52 +0800

    adding dependencies

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import Spinner from 'react-native-loading-spinner-overlay';
+import { app } from './firebase-config';
+import { getAuth } from 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const auth = getAuth();
+
+export default function App () {
+	const [user, loading] = useAuthState(auth);
+
+	return (
+		<PaperProvider theme={DefaultTheme}>
+			<Spinner visible={loading} color={DefaultTheme.colors.primary} />
+			<View style={styles.container}>
+				<Text>Open up App.js to start working on your app!</Text>
+				<StatusBar style="auto" />
+			</View>
+		</PaperProvider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
---
+++ screens/Login.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
+
+const Login = () => {
+	return (
+		<View>
+			<Text>Login</Text>
+		</View>
+	)
+}
+
+export default Login
+
+const styles = StyleSheet.create({})
