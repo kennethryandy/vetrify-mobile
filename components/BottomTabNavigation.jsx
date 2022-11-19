e96d752abf38610f449559100ac15e28a767c422
Kenneth Ryan <dykennethryan@gmail.com>
2022-11-19 19:19:16 +0800

    update tabs

--- components/BottomTabNavigation.jsx
+++ components/BottomTabNavigation.jsx
@@ -5,7 +5,6 @@ import Dashboard from '../screens/Dashboard';
 import { useTheme } from 'react-native-paper';
 import Profile from '../screens/Profile';
 import Pets from '../screens/Pets';
-import Chat from '../screens/Chat';
 import ChatList from '../screens/ChatList';
 import Appointment from '../screens/Appointment';
 
@@ -23,8 +22,8 @@ const BottomTabNavigation = () => {
 			screenOptions={({ route }) => ({
 				tabBarIcon: ({ focused, color }) => {
 					let iconName;
-					if (route.name === 'Dashboard') {
-						iconName = focused ? 'home-circle' : 'home-circle-outline';
+					if (route.name === 'Home') {
+						return <MaterialIcons name="pets" size={24} color={color} />
 					} else if (route.name === 'Profile') {
 						iconName = focused ? 'account-circle' : 'account-circle-outline';
 					} else if (route.name === 'Appointment') {
@@ -32,15 +31,15 @@ const BottomTabNavigation = () => {
 					} else if (route.name === 'ChatList') {
 						iconName = focused ? 'chat' : 'chat-outline';
 					} else if (route.name === 'Pets') {
-						return <MaterialIcons name="pets" size={24} color={color} />
+						iconName = focused ? 'dog' : 'dog';
 					}
 					return <MaterialCommunityIcons name={iconName} color={color} size={24} />
 				}
 			})}
 		>
-			<Tab.Screen name="Dashboard" component={Dashboard} />
 			<Tab.Screen name="Appointment" component={Appointment} />
 			<Tab.Screen name="Pets" component={Pets} />
+			<Tab.Screen name="Home" component={Dashboard} />
 			<Tab.Screen name="Profile" component={Profile} />
 			<Tab.Screen name="ChatList" component={ChatList} />
 		</Tab.Navigator>
